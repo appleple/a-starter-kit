564fcc65363261c7e9fbd41bfe40136428abba11
Kasumi Morita <morita@appleple.com>
2017-05-16 16:25:40 +0900

    Update index.js

--- themes/root/src/js/index.js
+++ themes/root/src/js/index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+// URL Dispatcherのサンプル
+//
+// const Dispatcher = require('url-dispatcher');
+// import samplePage from './sample';
+
+// const dispatcher = new Dispatcher({
+//     routes: {
+//         /**
+//          * サンプル
+//          */
+//         '/*': function () {
+//             samplePage(jQuery);
+//         },
+//     }
+// });
