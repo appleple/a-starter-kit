3be8d5d8715167d95f450a13dd0ce9112e999961
Hori Godai <ess_president@me.com>
2018-04-25 10:16:23 +0900

    reactとreact-domを追加

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,44 @@
  */
 const gulp = require('gulp');
 const rename = require('gulp-rename'),
-    sass = require('gulp-sass'),
-    path = require('path'),
-    cssnano = require('gulp-cssnano'),
-    watch = require('gulp-watch'),
-    autoprefixer = require('gulp-autoprefixer'),
-    csscomb = require('gulp-csscomb'),
-    replace = require('gulp-replace'),
-    package = require('./package.json');
+  sass = require('gulp-sass'),
+  path = require('path'),
+  cssnano = require('gulp-cssnano'),
+  watch = require('gulp-watch'),
+  autoprefixer = require('gulp-autoprefixer'),
+  csscomb = require('gulp-csscomb'),
+  replace = require('gulp-replace'),
+  package = require('./package.json');
 
 const rootTheme = package.config.theme;
-
 
 //テーマ
 //SCSSファイルをCSSにコンパイルする
 gulp.task('sass', () => {
-    gulp.src([`themes/${rootTheme}/src/scss/site.scss`])
-        .pipe(sass({
-          includePaths: ['node_modules']
-        }))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie 9'],
-            cascade: false
-        }))
-        .pipe(csscomb())// *1
-        .pipe(rename({
-            basename: 'bundle'
-        }))
-        .pipe(gulp.dest(`themes/${rootTheme}/dest`))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(cssnano({safe: true}))
-        .pipe(gulp.dest(`themes/${rootTheme}/dest`));
+  gulp.src([`themes/${rootTheme}/src/scss/site.scss`])
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'ie 9'],
+      cascade: false
+    }))
+    .pipe(csscomb())// *1
+    .pipe(rename({
+      basename: 'bundle'
+    }))
+    .pipe(gulp.dest(`themes/${rootTheme}/dest`))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(cssnano({ safe: true }))
+    .pipe(gulp.dest(`themes/${rootTheme}/dest`));
 });
 
 
 // プロジェクトのSCSSとCSSファイルを監視する
 gulp.task('project', () => {
-    gulp.watch(`themes/${rootTheme}/src/scss/**`,['sass']);
+  gulp.watch(`themes/${rootTheme}/src/scss/**`, ['sass']);
 });
 
 
---
+++ themes/root/src/js/component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (<p>hogehoge</p>);
+  }
+}
